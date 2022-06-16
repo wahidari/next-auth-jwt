59911194287345f64fd7f9e19310a7d209d3695f
wahidari <wahiid.ari@gmail.com>
2022-06-16 12:10:44 +0700

    Create login.js

---
+++ pages/api/login.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+export default function handler(req, res) {
+  const { username, password } = req.body;
+  const { method } = req;
+
+  if (!username || !password) {
+    return res.json({ status: 422, error: "Username and Password empty" });
+  }
+
+  switch (method) {
+    case 'POST':
+      if (username === 'admin' && password === 'admin') {
+        const token = jwt.sign({ id: 1, username: username }, process.env.JWT_SECRET );
+        res.status(200).json({ status: 200, token });
+      } else {
+        return res.json({ status: 422, error: "Username and Password not match" });
+      }
+      break;
+  }
+}
